539d1c5e9c56422a41ed3e56dae6b2ac38831f06
safwanvk <safwanvalakundil@gmail.com>
2020-06-22 14:22:53 +0530

    Set templates

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,16 @@
+from django.urls import path
 
+from .views import IndexView, \
+    BlogView, \
+    BlogDetailView, \
+    ContactView, \
+    AboutView
+
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index_view'),
+    path('blog/', BlogView.as_view(), name='blog_view'),
+    path('blog-detail/', BlogDetailView.as_view(), name='blog_detail'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('about/', AboutView.as_view(), name='about')
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+class BlogView(TemplateView):
+    template_name = "blog.html"
+
+
+class BlogDetailView(TemplateView):
+    template_name = "blog-single.html"
+
+
+class ContactView(TemplateView):
+    template_name = "contact.html"
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
