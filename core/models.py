dd080f8c5ff3115f3b6b6a71ce150a5d00212892
safwanvk <safwanvalakundil@gmail.com>
2020-06-22 15:51:31 +0530

    Create model Blog

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Blog(models.Model):
+    ILLUSTRATION = 'I'
+    BRANDING = 'B'
+    APPLICATION = 'A'
+    DESIGN = 'D'
+    MARKETING = 'M'
+    CATEGORY_CHOICES = [
+        (ILLUSTRATION, 'Illustration'),
+        (BRANDING, 'Branding'),
+        (APPLICATION, 'Application'),
+        (DESIGN, 'Design'),
+        (MARKETING, 'Marketing')
+    ]
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=200)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+    image = models.ImageField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
